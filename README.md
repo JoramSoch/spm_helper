ad6e092e6a84240a9f2de919338ebc4e1e28a03a
Joram Soch <joram.soch@bccn-berlin.de>
2020-07-22 14:44:18 +0200

    added calculate volumes
    The function "spm_calc_vols.m" allows to perform arbitrary operations on an arbitrary number of volumes.

--- README.md
+++ README.md
@@ -6,6 +6,7 @@
 
 In this repository, I collect some useful helper functions for Statistical Parametric Mapping (SPM):
 - `spm_analyze_RPs`: analyzes and plots movement parameters estimated by SPM
+- `spm_calc_vols`: performs arbitrary mathematical operations on several volumes
 - `spm_exec_mult_jobs`: executes multiple jobs created using the SPM batch editor
 - `spm_gen_con_paths`: generates contrast paths to be used in second-level analyses
 - `spm_save_thr_SPM`: saves thresholded statistical parametric maps for estimated GLM
@@ -13,8 +14,10 @@ In this repository, I collect some useful helper functions for Statistical Param
 
 Most of these functions are written such that they can be called with all input arguments, e.g.
 
-`spm_skull_strip('C:\VisRec\BIDS\sub-01\anat\sub-01_T1w.nii', 0.95)`,
+`spm_skull_strip('C:\BIDS\sub-01\anat\sub-01_T1w.nii', 0.95)`
+`[out, V] = spm_calc_vols('sum(V~=0)', {'C:\BIDS\sub-01\glms\glm-0\mask.nii'})`,
 
 or just using the function name, such that input arguments are specified via the SPM GUI, i.e.
 
-`spm_skull_strip`.
+`spm_skull_strip`
+`out = spm_calc_vols`.
---
+++ spm_calc_vols.m
@@ -0,0 +1,61 @@
+function [out, V] = spm_calc_vols(expr, vols)
+% _
+% Calculate Values from Volume or Volumes
+% FORMAT [out, V] = MF_analyze(expr, vols)
+% 
+%     expr - a string describing a mathematical operation on V
+%     vols - an n x 1 cell array specifying file paths of images
+% 
+%     out  - the result of the calculation described by expr
+%     V    - an n x v matrix with all volumes at all voxels
+% 
+% FORMAT out = MF_analyze(expr, vols) loads all the images specified by
+% vols, performs the operation indicated by expr and returns the result.
+% The string in expr must contain the capital letter V. All images need
+% to have the same dimensions.
+% 
+% FORMAT [out, V] = MF_analyze(expr, vols) also returns the image matrix.
+% 
+% Exemplary usage:
+%     f_mean = spm_calc_vols('mean(V)', {'C:\f0001.img'})
+% 
+% Author: Joram Soch, BCCN Berlin
+% E-Mail: joram.soch@bccn-berlin.de
+% 
+% First edit: 27/11/2014, 17:50
+%  Last edit: 22/07/2020, 14:24
+
+
+% Get expression if required
+%-------------------------------------------------------------------------%
+if nargin < 1 || isempty(expr)
+    expr = spm_input('Expression:', 1, 's', 'mean(V)');
+end;
+
+% Get paths if required
+%-------------------------------------------------------------------------%
+if nargin < 2 || isempty(vols)
+    vols = cellstr(spm_select(Inf, 'image', 'Please select volumes to analyze!', [], pwd, '.*', '1'));
+end;
+
+% Get matrix dimensions
+%-------------------------------------------------------------------------%
+v1_hdr = spm_vol(vols{1});
+v = prod(v1_hdr.dim);
+n = numel(vols);
+clear v1_hdr
+
+% Load all volumes
+%-------------------------------------------------------------------------%
+V = zeros(n,v);
+for i = 1:n
+    v_hdr = spm_vol(vols{i});
+    v_img = spm_read_vols(v_hdr);
+    v_img = reshape(v_img,[1 v]);
+    V(i,:)= v_img;
+end;
+clear v_hdr v_img
+
+% Determine result
+%-------------------------------------------------------------------------%
+eval(strcat('out = ',expr,';'));
