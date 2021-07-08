d3ef4c93f727fcd048245630a090bbb28bf12599
Joram Soch <joram.soch@bccn-berlin.de>
2021-07-08 05:26:42 +0200

    edited realignment parameters
    The function for analyzing realignment parameters now outputs the realignment parameters rather than their descriptive statistics.

--- spm_analyze_RPs.m
+++ spm_analyze_RPs.m
@@ -6,8 +6,8 @@ function [TPs, RPs] = spm_analyze_RPs(RP_file, RP_plot)
 %     RP_file - string indicating realignment parameter filename
 %     RP_plot - logical indicating whether to plot parameters
 % 
-%     TPs     - 3 x 3 matrix with translation parameter min, max & range
-%     RPs     - 3 x 3 matrix with rotation    parameter min, max & range
+%     TPs     - n x 3 matrix with translation parameters
+%     RPs     - n x 3 matrix with rotation parameters
 % 
 % FORMAT [TPs, RPs] = spm_analyze_RPs(RP_file, RP_plot) loads realignment
 % parameters and returns minima, maxima and ranges for translation as well
@@ -17,7 +17,7 @@ function [TPs, RPs] = spm_analyze_RPs(RP_file, RP_plot)
 % E-Mail: joram.soch@bccn-berlin.de
 % 
 % First edit: 19/02/2015, 06:45
-%  Last edit: 31/07/2020, 16:21
+%  Last edit: 27/04/2021, 11:10
 
 
 % Select file if required
@@ -35,30 +35,28 @@ if nargin < 2 || isempty(RP_plot), RP_plot = true; end;
 % Load realignment paramerters
 %-------------------------------------------------------------------------%
 RP_data = load(RP_file);
-[n p]   = size(RP_data);
+[n, p]  = size(RP_data);
 
 % Analyze realignment paramerters
 %-------------------------------------------------------------------------%
 RP_data(:,4:6) = RP_data(:,4:6) * (180/pi); % radians to degrees
-TPs = [min(RP_data(:,1:3)); max(RP_data(:,1:3))];
-RPs = [min(RP_data(:,4:6)); max(RP_data(:,4:6))];
-TPs = [TPs; (TPs(2,:)-TPs(1,:))];           % translations
-RPs = [RPs; (RPs(2,:)-RPs(1,:))];           % rotations
+TPs = RP_data(:,1:3);                       % translations
+RPs = RP_data(:,4:6);                       % rotations
 
 % Display realignment parameters
 %-------------------------------------------------------------------------%
 if RP_plot
     
-    figure;
+    figure('Name', 'Realignment Parameters', 'Color', [1 1 1], 'Position', [50 50 1600 900]);
     
     % Translations
     %---------------------------------------------------------------------%
     subplot(2,1,1);
     hold on;
-    plot([1:n],RP_data(:,1),'-b', 'LineWidth', 2);
-    plot([1:n],RP_data(:,2),'-g', 'LineWidth', 2);
-    plot([1:n],RP_data(:,3),'-r', 'LineWidth', 2);
-    xlim([1 n]);
+    plot([1:n],TPs(:,1),'-b', 'LineWidth', 2);
+    plot([1:n],TPs(:,2),'-g', 'LineWidth', 2);
+    plot([1:n],TPs(:,3),'-r', 'LineWidth', 2);
+    axis([(1-1), (n+1), -(11/10)*max(max(abs(TPs))), +(11/10)*max(max(abs(TPs)))]);
     grid on;
     set(gca,'Box','On');
     legend('x-axis', 'y-axis', 'z-axis', 'Location', 'NorthWest');
@@ -70,10 +68,10 @@ if RP_plot
     %---------------------------------------------------------------------%
     subplot(2,1,2);
     hold on;
-    plot([1:n],RP_data(:,4),'-b', 'LineWidth', 2);
-    plot([1:n],RP_data(:,5),'-g', 'LineWidth', 2);
-    plot([1:n],RP_data(:,6),'-r', 'LineWidth', 2);
-    xlim([1 n]);
+    plot([1:n],RPs(:,1),'-b', 'LineWidth', 2);
+    plot([1:n],RPs(:,2),'-g', 'LineWidth', 2);
+    plot([1:n],RPs(:,3),'-r', 'LineWidth', 2);
+    axis([(1-1), (n+1), -(11/10)*max(max(abs(RPs))), +(11/10)*max(max(abs(RPs)))]);
     grid on;
     set(gca,'Box','On');
     legend('pitch', 'roll', 'yaw', 'Location', 'NorthWest');
