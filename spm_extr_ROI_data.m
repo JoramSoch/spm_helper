468b63be4c616011addc775229b8e9c4d2651ac4
Joram Soch <joram.soch@bccn-berlin.de>
2021-08-25 10:47:42 +0200

    edited ROI extraction
    A third input argument "meth" (for extraction method) was added to the function for extracting ROI averages from data images

--- spm_extr_ROI_data.m
+++ spm_extr_ROI_data.m
@@ -1,25 +1,35 @@
-function Y = spm_extr_ROI_data(data, ROIs)
+function Y = spm_extr_ROI_data(data, ROIs, meth)
 % _
 % Extract ROI Averages from Data Images
-% FORMAT Y = spm_extr_ROI_data(data, ROIs)
+% FORMAT Y = spm_extr_ROI_data(data, ROIs, meth)
 % 
 %     data - an n x 1 cell array with file paths to data images
 %     ROIs - an r x 1 cell array with file paths to ROI images
+%     meth - a string indicating the extraction method (see below)
 % 
 %     Y    - an n x r matrix with ROI averages
 % 
-% FORMAT Y = spm_extr_ROI_data(data, ROIs) loads data images as well as
-% region of interest images and returns ROI-wise averages from each data
-% volume.
+% FORMAT Y = spm_extr_ROI_data(data, ROIs, meth) loads data images as well
+% as region of interest images (ROIs) and returns ROI-wise averages from
+% each data volume using the option meth.
 % 
 % FORMAT Y = spm_extr_ROI_data; uses the SPM file selector, such that the
 % user can enter data and ROI images via the GUI.
+% 
+% Note that all data images and ROI images need to have the same dimensions,
+% such that ROI voxels can be identified in the data volumes.
+% 
+% The input variable "meth" is a string indicating the extraction method:
+% - 'mean'   : calculate average across all voxels within ROI
+% - 'NaNmean': calculate average across ROI voxels, omitting NaN values
+% - further options such as 'PCA' and 'eigenvariate' will be implemented
+% The default value for this variable is 'NaNmean'.
 % 
 % Author: Joram Soch, BCCN Berlin
 % E-Mail: joram.soch@bccn-berlin.de
 % 
 % First edit: 02/07/2021, 15:53
-%  Last edit: 02/07/2021, 16:35
+%  Last edit: 25/08/2021, 10:44
 
 
 % Get paths if required
@@ -29,6 +39,9 @@ if nargin < 1 || isempty(data)
 end;
 if nargin < 2 || isempty(ROIs)
     ROIs = cellstr(spm_select(Inf, 'image', 'Please select ROI images to extract with!', [], pwd, '.*', '1'));
+end;
+if nargin < 3 || isempty(meth)
+    meth = 'NaNmean';
 end;
 
 % Get image dimensions
@@ -65,7 +78,11 @@ for i = 1:n
     d_hdr = spm_vol(data{i});
     d_img = spm_read_vols(d_hdr);
     for j = 1:r
-        Y(i,j) = mean(d_img(R(j,:)~=0),'omitnan');
+        if strcmp(meth,'mean')
+            Y(i,j) = mean(d_img(R(j,:)~=0));
+        elseif strcmp(meth,'NaNmean')
+            Y(i,j) = mean(d_img(R(j,:)~=0),'omitnan');
+        end;
     end;
     spm_progress_bar('Set',(i/n)*100);
 end;
